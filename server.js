3dedcb70205a56d9a284f594bb9f65914b5d49ce
Sumit Khanduri <sumitk@walshintegrated.com>
2024-05-21 21:15:16 -0400

    connected mongodb

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose = require(`mongoose`);
+
 const app = express();
 const port = 3000;
-app.get('/', (req,res)=>res.send(`I love you ${port}`))
+const pwd = 'root'
+const username = 'root';
 
+app.get('/', (req,res)=>res.send(`I love you ${port}`))
 app.get('/new', (req,res)=>res.send(`new ${port}`))
 
-app.listen(3000, () => {
-    console.log(`Node API app is running on port ${port}`);
-})
+
+mongoose.set("strictQuery", false);
+mongoose.connect(`mongodb+srv://${username}:${pwd}@khanduriboycluster.psqmknc.mongodb.net/Node-API?retryWrites=true&w=majority&appName=khanduriBoyCluster`).then(
+    () => {
+        console.log(`connected to mongoDB`);
+        app.listen(3000, () => {
+            console.log(`Node API app is running on port ${port}`);
+        })
+    }
+
+).catch(err => console.log(err));
